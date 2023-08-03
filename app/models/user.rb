f4719a48dde6d29f3d272a80241e384e0e15038d
kobakon <138418858+kobakon@users.noreply.github.com>
2023-08-03 06:48:01 +0000

    [Start]応用課題４

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "user_id,", dependent: :destroy
+  has_many :yyy, through: :relationships, sourcu: :users
+  
   has_one_attached :profile_image
 
   
