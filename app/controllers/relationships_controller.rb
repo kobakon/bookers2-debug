85c8ce06fb0473b5bd34739ad65da1090b3f8c8a
kobakon <138418858+kobakon@users.noreply.github.com>
2023-08-04 07:04:20 +0000

    [Finish]フォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,29 @@
 class RelationshipsController < ApplicationController
-  
+
+
+  # フォローする
   def create
-    
+    follower = current_user.relationships.build(followed_id: params[:user_id])
+    follower.save!
+    redirect_to request.referer
   end
-  
+
+  # フォロー外す
   def destroy
+    follower = current_user.relationships.find_by(followed_id: params[:user_id])
+    follower.destroy
+    redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
     
   end
   
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  has_many :relationships, class_name: "Relationship", foreign_key: "user_id,", dependent: :destroy
-  has_many :yyy, through: :relationships, sourcu: :users
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
   
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
   
@@ -24,5 +26,9 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
